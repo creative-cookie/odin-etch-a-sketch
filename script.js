5a514c2fce9a1afff0406abfba1af548c3a71d9e
creative-cookie <38289561+creative-cookie@users.noreply.github.com>
2023-07-02 18:13:06 -0400

    add user defined grid size functionality

--- script.js
+++ script.js
@@ -3,10 +3,18 @@ let gridSize = 16;
 
 const gridContainer = document.getElementById("grid-container");
 const clearGridBtn = document.getElementById("clear-grid-btn");
+const generateGridBtn = document.getElementById("generate-grid-btn");
+const gridSizeInput = document.getElementById("grid-size-input");
+
+// Add Event Listeners
+clearGridBtn.addEventListener("click", clearGrid);
+generateGridBtn.addEventListener("click", updateGrid);
+gridSizeInput.addEventListener("keyup", formValidation);
 
 function square(num){
     return num * num;
 }
+
 function generateGrid(gridSize){
     for(let i = 0; i < square(gridSize); i++){
         const newBox = document.createElement("div");
@@ -16,8 +24,32 @@ function generateGrid(gridSize){
         newBox.addEventListener("mouseenter", changeColor);
         gridContainer.appendChild(newBox);
     }
+}
 
-    clearGridBtn.addEventListener("click", clearGrid);
+function updateGrid(e){
+    e.preventDefault(); //stop form from submitting
+    e.currentTarget.blur(); // remove btn focus state after click
+    if (gridSizeInput.value >= 1 && gridSizeInput.value <= 100){
+        removeGrid(); // remove old grid
+        generateGrid(gridSizeInput.value); //generate new grid using user value
+    }
+}
+
+function removeGrid(){
+    let oldBox = gridContainer.lastElementChild;
+    while(oldBox){
+        oldBox.removeEventListener("mouseenter",changeColor);
+        gridContainer.removeChild(oldBox);
+        oldBox = gridContainer.lastElementChild;
+    }
+}
+
+function formValidation(e){
+    if(isNaN(gridSizeInput.value) || gridSizeInput.value > 100 || gridSizeInput.value < 1){
+        gridSizeInput.classList.add("input-error");
+    } else{
+        gridSizeInput.classList.remove("input-error");
+    }
 }
 
 function changeColor(e){
